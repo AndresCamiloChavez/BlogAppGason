5d028609915e5af7c2c16abe00f7bdfc6416f21a
Andres Chavez <andres.cz.chavez@gmail.com>
2022-02-05 21:12:54 -0500

    Descripción en el post item

--- app/src/main/java/com/devandreschavez/blogappgason/ui/adapter/HomeAdapter.kt
+++ app/src/main/java/com/devandreschavez/blogappgason/ui/adapter/HomeAdapter.kt
@@ -5,27 +5,37 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.devandreschavez.blogappgason.core.BaseViewHolder
+import com.devandreschavez.blogappgason.core.hide
 import com.devandreschavez.blogappgason.data.model.Post
 import com.devandreschavez.blogappgason.databinding.PostItemViewBinding
 
-class HomeAdapter(private val listPosts: List<Post>): RecyclerView.Adapter<BaseViewHolder<*>>() {
+class HomeAdapter(private val listPosts: List<Post>) : RecyclerView.Adapter<BaseViewHolder<*>>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        val itemBinding = PostItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val itemBinding =
+            PostItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return PostViewHolder(itemBinding)
     }
+
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        when(holder){
+        when (holder) {
             is PostViewHolder -> holder.bind(listPosts[position])
         }
     }
+
     override fun getItemCount(): Int = listPosts.size
 
-    inner class PostViewHolder(private val binding: PostItemViewBinding): BaseViewHolder<Post>(binding.root){
+    inner class PostViewHolder(private val binding: PostItemViewBinding) :
+        BaseViewHolder<Post>(binding.root) {
         override fun bind(item: Post) {
             Glide.with(binding.root.context).load(item.profilePicture).into(binding.profilePicture)
             Glide.with(binding.root.context).load(item.postImage).into(binding.postImage)
             binding.profileName.text = item.profileName
             binding.postTimestamp.text = "Hace 2 horas"
+            if (item.postDescription.isEmpty()) {
+                binding.tvDescription.hide()
+            } else {
+                binding.tvDescription.text = item.postDescription
+            }
         }
 
     }
