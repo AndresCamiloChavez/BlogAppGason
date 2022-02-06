0cfe281ecda233bfd0f84f183f3f61451029ae98
Andres Chavez <andres.cz.chavez@gmail.com>
2022-02-05 20:58:24 -0500

    Control del navigation en login y en register

--- app/src/main/java/com/devandreschavez/blogappgason/MainActivity.kt
+++ app/src/main/java/com/devandreschavez/blogappgason/MainActivity.kt
@@ -1,24 +1,13 @@
 package com.devandreschavez.blogappgason
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.devandreschavez.blogappgason.databinding.ActivityMainBinding
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import java.io.ByteArrayOutputStream
-import java.util.*
+
+import com.devandreschavez.blogappgason.core.hide
+import com.devandreschavez.blogappgason.core.show
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -29,5 +18,19 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when(destination.id){
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
     }
 }
